26e9bd761ab23f8d0e57c64d6eb742e6c338e518
James Ding <git@twango.dev>
2026-08-03 14:37:00 -0700

    chore(web): rely on runes inference

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -17,11 +17,6 @@ export default defineConfig({
 		postframeWasm(),
 		tailwindcss(),
 		sveltekit({
-			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
-				runes: ({ filename }) =>
-					filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-			},
 			adapter: adapter()
 		})
 	]
