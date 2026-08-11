d3a1267f106831a2ca46f46bfb38b01f3bbe1d14
James Ding <git@twango.dev>
2026-08-11 13:45:34 -0700

    feat(web): add rybbit analytics in production

--- web/src/routes/+layout.svelte
+++ web/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { dev } from '$app/environment';
 	import './app.css';
 	import favicon from '$lib/assets/favicon.svg';
 
@@ -11,6 +12,13 @@
 		name="description"
 		content="Postframe is a focused workspace for organizing, developing, and finishing photographs."
 	/>
+	{#if !dev}
+		<script
+			src="https://rybbit.twango.dev/api/script.js"
+			data-site-id="b45b9596157c"
+			defer
+		></script>
+	{/if}
 </svelte:head>
 
 {@render children()}
